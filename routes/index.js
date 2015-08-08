5482d9abaaa08df3a37af848e401b2da73add733
xiaolulu <jiangxiao2000@126.com>
2015-08-08 14:58:35 +0800

    replay

--- routes/index.js
+++ routes/index.js
@@ -23,6 +23,37 @@ Domain.on( 'error', function( e ){
 
 var xmlStr = '<xml><ToUserName><![CDATA[toUser]]></ToUserName><FromUserName><![CDATA[fromUser]]></FromUserName><CreateTime>1351776360</CreateTime><MsgType><![CDATA[link]]></MsgType><Title><![CDATA[公众平台官网链接]]></Title><Description><![CDATA[公众平台官网链接]]></Description><Url><![CDATA[url]]></Url><MsgId>1234567890123456</MsgId></xml> ';
 
+var path = {
+	'text': 'text',
+	'event': 'event'
+}
+
+var tmp = {
+	text: ['<xml>',
+			'<ToUserName><![CDATA[{toUser}]]></ToUserName>',
+			'<FromUserName><![CDATA[{fromUser}]]></FromUserName>',
+			'<CreateTime>{12345678}</CreateTime>',
+			'<MsgType><![CDATA[{text}]]></MsgType>',
+			'<Content><![CDATA[{content}]]></Content>',
+			'</xml>'].join('')
+}
+
+var replay = {};
+
+replay.text = function( req, res, config ){
+	var data = {
+		toUser: config.FromUserName,
+		fromUser: config.fromUser,
+		content: '你好'
+	}
+	var _html = tmp['text'].replace( /\{(.*?)\}/g, function( $1, $2 ){
+				return data[ $2 ];
+			});
+
+	res.send( _html );
+
+}
+
 exports.all = function( app ){
     app.use( function( req, res, next){	
 		var bufferData = new BufferHelper();
@@ -34,7 +65,8 @@ exports.all = function( app ){
 		req.on( 'end', function(){
 			var xmlStr = bufferData.toBuffer().toString();
 			xml2js.parseString(xmlStr, { explicitArray : false, ignoreAttrs : true }, function (err, result) {
-				console.log(JSON.stringify(result));
+				var ret = JSON.stringify(result);
+				replay[ ret.xml.MsgType ]( req, res, ret.xml ); 
 			})
 		})
 		next();
