039dce843aa596828dea93d3f1ac5404a0cce375
xiaolulu <jiangxiao2000@126.com>
2015-08-24 16:13:06 +0800

    welcome

--- assets/module/issue/welcome/welcome.js
+++ assets/module/issue/welcome/welcome.js
@@ -12,5 +12,10 @@ define( [ 'mdialog', 'all'], function( Mdialog ){
 	$( '#corpname' ).html( companyName || '仟金顶网络科技' ); 
 
 	var mdialog = new Mdialog();
-	mdialog.show( '会员申请成功!<br />小仟将尽快为您提供授信服务！' );
+	if( location.hash == '#ready' ){
+		mdialog.show( '服务平台正在施工!<br />先去逛逛吧！' );
+	} else {
+		mdialog.show( '会员申请成功!<br />小仟将尽快为您提供授信服务！' );
+	}
+	
 });
